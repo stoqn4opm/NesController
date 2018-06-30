25f5903335e7434e107e917a00e8357ff7b1b001
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2018-06-30 10:40:31 +0100

    some progress with freeRTOS bullshit

--- firmware_controller/firmware_controller.ino
+++ firmware_controller/firmware_controller.ino
@@ -15,7 +15,7 @@ namespace {
 	TaskHandle_t statusTask;
 	TaskHandle_t readInputTask;
 
-	HEF4021BP shiftRegister(3, 4, 5); // clock, latch, data
+	HEF4021BP shiftRegister(26, 27, 28); // clock, latch, data
 
 	SemaphoreHandle_t hasNewDataSignal = 0;
 	SemaphoreHandle_t gateKeeperShiftRegister = 0;
@@ -25,21 +25,28 @@ namespace {
 
 void setup() {
 
+	if (gateKeeperShiftRegister == NULL) {
+		gateKeeperShiftRegister = xSemaphoreCreateMutex();
+		if (gateKeeperShiftRegister != NULL) {
+			xSemaphoreGive(gateKeeperShiftRegister);
+		}
+	}
+
 	hasNewDataSignal = xSemaphoreCreateBinary();
-	gateKeeperShiftRegister = xSemaphoreCreateMutex();
 
 	pinMode(CONNECTION_CHECK_PIN, INPUT);
     pinMode(MODE_CONTROL_KEY_PIN, OUTPUT);
     pinMode(POWER_CONTROL_PIN, OUTPUT);
-    
 
 	if (!AVRUserDefaults::isBluetoothAlreadyConfigured()) {
-		xTaskCreate(BluetoothInit, "1", 100, NULL, 1, &bluetoothInitTask);
+		xTaskCreate(BluetoothInit, "1", 120, NULL, 3, &bluetoothInitTask);
 	} else {
-		xTaskCreate(BluetoothCommunicate, "3", 120, NULL, 2, &bluetoothCommunicationTask);  
-		xTaskCreate(ReadInput, 			  "4", 120, NULL, 1, &readInputTask);
+		// CommonBluetooth::enterMode(MODE_NORMAL);
+		// Serial.begin(BAUD_RATE_NORMAL);
+		// xTaskCreate(BluetoothCommunicate, "3", 1450, NULL, 2, &bluetoothCommunicationTask);  
+		// xTaskCreate(ReadInput, 			  "4", 1450, NULL, 1, &readInputTask);
 	}
-	xTaskCreate(Status, "2", 120, NULL, 1, &statusTask);
+	xTaskCreate(Status, "2", 1000, NULL, 1, &statusTask);
 }
 
 void loop() { } 
@@ -47,27 +54,36 @@ void loop() { }
 
 //MARK: - Bluetooth Tasks
 
-static void BluetoothInit(void* pvParameters) {
+static void BluetoothInit(void* pvParameters __attribute__((unused))) {
 
-	bool result = SlaveBluetooth::initModule();
-	while (result == false) {
-		CommonBluetooth::enterMode(MODE_SLEEP);
-		vTaskDelay(1000);
-		result = SlaveBluetooth::initModule();
+	while(1) {
+		bool result = SlaveBluetooth::initModule();
+		while (result == false) {
+			CommonBluetooth::enterMode(MODE_SLEEP);
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			result = SlaveBluetooth::initModule();
+		}
+
+		CommonBluetooth::enterMode(MODE_NORMAL);
+		Serial.flush();
+		Serial.end();
+		Serial.begin(BAUD_RATE_NORMAL);
+
+		xTaskCreate(ReadInput, 			  "4", 1450, NULL, 1, &readInputTask);
+		xTaskCreate(BluetoothCommunicate, "3", 1450, NULL, 2, &bluetoothCommunicationTask);
+		vTaskDelete(bluetoothInitTask);
 	}
-	xTaskCreate(BluetoothCommunicate, "3", 120, NULL, 2, &bluetoothCommunicationTask);  
-	xTaskCreate(ReadInput, 			  "4", 120, NULL, 1, &readInputTask);
-
-	vTaskDelete(NULL);
 }
 
-static void BluetoothCommunicate(void* pvParameters) {
+static void BluetoothCommunicate(void* pvParameters __attribute__((unused))) {
 
-	    CommonBluetooth::enterMode(MODE_NORMAL);
-	    Serial.begin(BAUD_RATE_NORMAL);
+	// Serial.begin(38400);
+    	// Serial.println("Task3 Running, Creating Task2 and Task4");	    
 	while(1) {
-		if (xSemaphoreTake(hasNewDataSignal, portMAX_DELAY)) {
-			if (xSemaphoreTake(gateKeeperShiftRegister, 200)) {
+		
+		if (xSemaphoreTake(hasNewDataSignal, portMAX_DELAY) == pdTRUE) {
+			Serial.println("in communicate has hasNewDataSignal");
+			if (xSemaphoreTake(gateKeeperShiftRegister, 200) == pdTRUE) {
 				int8_t buttonStates = shiftRegister.getButtonStates();
 				xSemaphoreGive(gateKeeperShiftRegister);
 				CommonBluetooth::BluetoothPacket packet;
@@ -82,31 +98,35 @@ static void BluetoothCommunicate(void* pvParameters) {
 
 //MARK: - Status Update Task
 
-static void Status(void* pvParameters) {
+static void Status(void* pvParameters __attribute__((unused))) {
 
 	while(1) {
-
-		if (xSemaphoreTake(gateKeeperShiftRegister, 200)) {
-			int8_t buttonStates = shiftRegister.getButtonStates();
-			xSemaphoreGive(gateKeeperShiftRegister);
-			AVRPowerManager::shared()->shutdownIfNeeded(buttonStates);
-		}  
+		// Serial.println(F("in status"));
+		// if (xSemaphoreTake(gateKeeperShiftRegister, 200) == pdTRUE) {
+		// 	int8_t buttonStates = shiftRegister.getButtonStates();
+		// 	xSemaphoreGive(gateKeeperShiftRegister);
+		// 	AVRPowerManager::shared()->shutdownIfNeeded(buttonStates);
+		// }  
 	}
 }
 
 //MARK: - Read Input Task
 
-static void ReadInput(void* pvParameters) {
+static void ReadInput(void* pvParameters __attribute__((unused))) {
+
+	static int8_t lastStates = 0;
 
 	while(1) {
-
-		if (xSemaphoreTake(gateKeeperShiftRegister, 200)) {
-			int8_t buttonStates = shiftRegister.getButtonStates();
+		if (xSemaphoreTake(gateKeeperShiftRegister, 200) == pdTRUE) {
+			
 			shiftRegister.update();
 			int8_t newButtonStates = shiftRegister.getButtonStates();
 			xSemaphoreGive(gateKeeperShiftRegister);
 
-			if (buttonStates != newButtonStates) {
+			if (lastStates != newButtonStates) {
+
+				Serial.println("different");
+				lastStates = newButtonStates;
 				xSemaphoreGive(hasNewDataSignal);
 			}
 		}
